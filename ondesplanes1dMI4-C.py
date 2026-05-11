d955e1bcbdb2273b6754670e83ad9ff5c5ab6839
Néo CARDOT-BUTZ <neo.cardot-butz@etu.cyu.fr>
2026-05-11 16:38:18 +0200

    refonte du code pour un code + jolie et fonctionnel. Ajout de la correction du prof

--- ondesplanes1dMI4-C.py
+++ ondesplanes1dMI4-C.py
@@ -3,40 +3,49 @@ import matplotlib.pyplot as plt
 
 from numpy import pi,exp,sqrt,real,imag,zeros,linspace,cos
 
-def tracer_graphique(x, y, t):
-    """Trace un graphique en fonction des valeurs y en entrée et de t"""
-    #q1
-    plt.plot(x, y.real, label=f"Re(Ψ)", color="blue", linewidth=2)
-    #plt.plot(x, y[0].imag, label=f"Im(Ψ)", color="red", linestyle="--", alpha=0.7)
-    # q2
-    psi0 = 1
-    k = 5.0
-    delta_k = 0.5
-    # on calcule directement les trois fonctions dans le graphique pour plus de simplicité mais il faudrait le faire à part normalement.
-    psi1 = psi0 * np.cos(k * x)
-    psi2 = (psi0 / 2) * cos((k + delta_k) * x)
-    psi3 = (psi0 / 2) * cos((k - delta_k) * x)
-    psi_tot = psi1 + psi2 + psi3
+TWO_PI = 2*pi
+M = 9.2e-31 # masse d'une e-
+H_BARRE = 6.63e-34
 
-    # affiche sur le meme graphique les 3 fonctions
-    plt.plot(x, psi1.real, label=f"Re(Ψ_1)", color="red", linewidth=2)
-    plt.plot(x, psi2.real, label=f"Re(Ψ_2)", color="yellow", linewidth=2)
-    plt.plot(x, psi3.real, label=f"Re(Ψ_3)", color="green", linewidth=2)
-    plt.plot(x, psi_tot.real, label=f"Re(Ψ_tot)", color="black", linewidth=2)
+def tracer_graphique(x, t, waves : np.ndarray, k_list : list) -> None:
+    """Trace un graphique en fonction des valeurs y en entrée et de t"""
+    if (len(k_list) <= 1) :
+        print("Calculer plus d'une fonction pour voir la somme")
+        return
     
-    plt.xlabel("Position x")
-    plt.ylabel("Amplitude")
-    plt.title(f"Fonction d'onde Ψ(x,t) à t={t}")
-    plt.legend()
-    plt.grid(True, alpha=0.3)
-    plt.axhline(0, color='black', linewidth=1) # Axe horizontal
+    colors = ["purple","blue","green","yellow","red"]
+
+    fig, axes = plt.subplots(len(k_list)+1, 1, figsize=(8, 10))
+    
+    for i in range(len(k_list)):
+        axes[i].plot(x, waves[i].real, color=colors[i % len(colors)])
+        axes[i].set_title(f"Re(Ψ_{i+1})")
+        axes[i].set_ylabel(f"k={k_list[i]:.3g}m^-1\n" f"λ={(TWO_PI/k_list[i]):1g}m")
+        axes[i].grid(True)
+    
+    axes[-1].plot(x, waves[-1].real, color='blue')
+    axes[-1].set_title("Re(Ψ_tot)")
+    axes[-1].set_ylabel(f"λ={(TWO_PI/k_list[-1]):1g}m")
+    axes[-1].grid(True)
+
+    plt.tight_layout()
     plt.show()
 
-# retourne l'onde plane
+# retourne toutes les ondes planes
 def PlaneWave(amp : float , k : float , omega : float, x : np.ndarray, t : float) -> complex : 
     """Return a plane wave in 1d time reliant"""
-    return amp*exp(1j*(omega * t - k * x)) 
+    return amp*exp(1j*(k*x - omega * t))
 
+def SumPlanesWaves(psi : np.ndarray) -> np.ndarray:
+    """Return the sum of the previous waves functions"""
+    return np.sum(psi,axis=0)
+
+def calcul_omega(k : list) :
+    if (len(k) <= 0 ) : 
+        print("Le nombre de k ne peux pas être négatif")
+        return ValueError
+    return H_BARRE/2*M*np.power(k,2)
+    
 # on définit une fonction "main()", qui sert de base au programme
 def main():
 
@@ -45,20 +54,38 @@ def main():
         # input() sert à lire la donnée d'entrée (comme scanf())
         t = int(input("Entrer la valeur de t : "))
         amp = float(input("Entrer la valeur de l'amplitude : "))
-        omega = float(input("Entrer la valeur de la pulsation propre : "))
-        k = float(input("Entrer la valeur de k : "))
-        dk = float(input("Entrer la valeur de Δk : "))
     except ValueError: # si il y a une erreur de type on rentre dans le bloque
         print("Erreur : veuillez entrer des nombres valides.")
         return
     
-    nb_points = 200 # densité d'affichage (+ il y en a + la courbe est lisse)
+    nb_points = 500 # densité d'affichage (+ il y en a + la courbe est lisse)
 
-    x = np.linspace(-pi/dk, pi/dk, nb_points) # calcul de l'axe x sur [-pi/delta_k,pi/delta_k]
+    k_centre, dk = 5, 0.1
 
-    y = PlaneWave(amp, k, omega, x, t) # calcul de l'axe y (grace à numpy on peut directement envoyer un tableau et numpy fait le calcul à notre place)
-    
-    tracer_graphique(x,y,t) # on trace le graphique
+    x_debut, x_fin = -50, 50 # changer l'intervalle
+
+    x = np.linspace(x_debut,x_fin,nb_points) # calcul de l'axe x sur [0,100]
+
+    k = [
+        k_centre - 2*dk,
+        k_centre,
+        k_centre + 2*dk
+    ]
+
+    n_waves = 3
+    if (n_waves > len(k)): n_waves = len(k)
+
+    omega = calcul_omega(k)
+
+    waves = [PlaneWave(amp,k[i],omega[i],x,t) for i in range(n_waves)] # calcul de l'axe y (grace à numpy on peut directement envoyer un tableau et numpy fait le calcul à notre place)
+    waves.append(SumPlanesWaves(waves))
+
+    print("\n"*10)
+    print("----------------Rappel des conditions initiales----------------")
+    print(f"              k = {k}")
+    print(f"              w = {omega}")
+
+    tracer_graphique(x, t, waves, k) # on trace le graphique
 
 if __name__ == "__main__": # on appelle la fonction "main()"
     main()
